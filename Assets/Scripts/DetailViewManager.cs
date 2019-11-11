5871fd6d84e6f9632ce28fbdc30fa5e924e6ac96
Youngjae Park <54255680+piglife92@users.noreply.github.com>
2019-11-11 14:10:03 +0900

    3번째 화면제거
    3번째 화면 제거 및 관련 scripts 변경

--- Assets/Scripts/DetailViewManager.cs
+++ Assets/Scripts/DetailViewManager.cs
@@ -30,8 +30,7 @@ public class DetailViewManager : ViewManager
     [SerializeField] InputField phoneNumberInputField;
     [SerializeField] InputField emailInputField;
     [SerializeField] Button saveButton;
-    [SerializeField] GameObject thirdViewPrefab;
-
+    
     public delegate void DetailViewManagerSaveDelegate(Contact contact);
     public DetailViewManagerSaveDelegate saveDelegate;
 
@@ -107,13 +106,5 @@ public class DetailViewManager : ViewManager
         saveDelegate?.Invoke(newContact);
 
         ToggleEditMode(true);
-    }
-
-    public void Next()
-    {
-        ThirdViewManager thirdViewManager = 
-            Instantiate(thirdViewPrefab).GetComponent<ThirdViewManager>();
-
-        mainManager.PresentViewManager(thirdViewManager);
     }
 }
